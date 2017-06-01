8730d7ef3625e4b23f17f001ab80e60ac8df7524
yepstepz <prettyoptimist@gmail.com>
2017-06-01 17:45:08 +0300

    learning DS theory

--- src/data_structures_04/ds_theory.js
+++ src/data_structures_04/ds_theory.js
@@ -1,6 +1,6 @@
 let fulljournal = require('./ds_data.js');
 let journal = [];
-function addEntry(events, didITurnIntoASquirrel){
+function addEntry(events, didITurnIntoASquirrel) {
     journal.push({
         day: events,
         didITurnIntoASquirrel: didITurnIntoASquirrel
@@ -9,7 +9,6 @@ function addEntry(events, didITurnIntoASquirrel){
 addEntry(["работа", "тронул дерево", "пицца", "пробежка", "телевизор"], false);
 addEntry(["работа", "мороженое", "цветная капуста", "лазанья", "тронул дерево", "почистил зубы"], false);
 addEntry(["выходной", "велик", "перерыв", "арахис", "пивасик"], true);
-console.log(journal);
 function phi(table) {
     return (table[3] * table[0] - table[2] * table[1]) /
         Math.sqrt((table[2] + table[3]) *
@@ -17,4 +16,45 @@ function phi(table) {
             (table[1] + table[3]) *
             (table[0] + table[2]));
 }
-console.log(fulljournal);
+function existIn(array, item) {
+    return array.indexOf(item) == -1 ? false : true;
+}
+function countEntries(fullJournal, entry) {
+    let table = [0, 0, 0, 0]; // ff, tf, ft, tt
+    for (let post in fullJournal) {
+        let index = 0;
+        if (existIn(fullJournal[post].events, entry)) index += 2;
+        if (fullJournal[post].squirrel) index += 1;
+        table[index] += 1;
+    }
+    return table;
+}
+function allDeals(fulljournal) {
+    let list = new Set([]);
+    for (let post in fulljournal) {
+        for (let i = 0; i < fulljournal[post].events.length; i++) {
+            list.add(fulljournal[post].events[i]);
+        }
+    }
+    return list;
+}
+function allPhi(fulljournal, list) {
+    list = [...list];
+    let phies = {};
+    for (let i = 0; i < list.length; i++) {
+        phies[list[i]] = phi(countEntries(fulljournal, list[i]));
+    }
+    return phies;
+}
+for (let post in fulljournal) {
+    if (existIn(fulljournal[post].events, 'peanuts') &&
+        !existIn(fulljournal[post].events, 'brushed teeth')) {
+        fulljournal[post].events.push('peanuts but not brushed teeth');
+    }
+}
+let phies = allPhi(fulljournal, allDeals(fulljournal));
+for (let phi in phies) {
+    if (phies[phi] > 0.1 || phies[phi] < -0.1) {
+        console.log(phi + ':' + phies[phi]);
+    }
+}
